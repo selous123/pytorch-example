2d8feb3dc11e66ac7bee6ba838a3ebd1cf1005f6
selous123 <lrhselous@163.com>
2017-11-20 17:43:43 +0800

    modify config

--- mnist_cnn/model/model.py
+++ mnist_cnn/model/model.py
@@ -11,13 +11,13 @@ class Net(nn.Module):
         super(Net, self).__init__()
         # 1 input image channel, 6 output channels, 5x5 square convolution
         # kernel
-        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv1 = nn.Conv2d(1, 6, 5,padding=(2,2))
         self.bn1 = nn.BatchNorm2d(6)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5,padding=(2,2))
         self.bn2 = nn.BatchNorm2d(16)
         # an affine operation: y = Wx + b
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc1 = nn.Linear(16 * 7 * 7, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
