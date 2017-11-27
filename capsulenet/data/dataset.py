48f51f5524f137cb4befda45a646ad7dc155db8e
lrhselous@163.com <lrhselous@163.com>
2017-11-27 16:25:40 +0800

    add augmentation

--- capsulenet/data/dataset.py
+++ capsulenet/data/dataset.py
@@ -12,6 +12,18 @@ def dense_to_one_hot(labels,class_num):
     a[np.arange(height),labels] = 1
     return a
 
+def augmentation(x, max_shift=2):
+    _, _, height, width = x.size()
+
+    h_shift, w_shift = np.random.randint(-max_shift, max_shift + 1, size=2)
+    source_height_slice = slice(max(0, h_shift), h_shift + height)
+    source_width_slice = slice(max(0, w_shift), w_shift + width)
+    target_height_slice = slice(max(0, -h_shift), -h_shift + height)
+    target_width_slice = slice(max(0, -w_shift), -w_shift + width)
+
+    shifted_image = np.zeros(*x.size())
+    shifted_image[:, :, source_height_slice, source_width_slice] = x[:, :, target_height_slice, target_width_slice]
+    return shifted_image.float()
 ##read the whole file
 class mnistData(data.Dataset):
     """
@@ -23,13 +35,15 @@ class mnistData(data.Dataset):
         self.root,self.train,self.data,self.labels
         
     """
-    def __init__(self,root,train):
+    def __init__(self,root,train,augmentation=True):
         self.root = root
         self.train = train
         #train dataset
         if self.train:
             self.train_data,self.train_labels = input_data.read_train_data(self.root)
             self.train_labels = dense_to_one_hot(self.train_labels,10)
+            if augmentation:
+                self.train_data = augmentation(self.train_data / 255.0)
         #test dataset
         else:
             self.test_data,self.test_labels = input_data.read_test_data(self.root)
