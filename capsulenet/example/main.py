5043fdb9a6e8da32b046e2bed99c373eeba8a6c6
selous123 <lrhselous@163.com>
2017-11-27 14:27:56 +0800

    modify config file

--- capsulenet/example/main.py
+++ capsulenet/example/main.py
@@ -34,10 +34,10 @@ dataset_transform = transforms.Compose([
                        transforms.Normalize((0.1307,), (0.3081,))
                    ])
 
-train_dataset = datasets.MNIST('../data', train=True, download=True, transform=dataset_transform)
+train_dataset = datasets.MNIST('./data', train=True, download=True, transform=dataset_transform)
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
-test_dataset = datasets.MNIST('../data', train=False, download=True, transform=dataset_transform)
+test_dataset = datasets.MNIST('./data', train=False, download=True, transform=dataset_transform)
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=test_batch_size, shuffle=True)
 
 #
